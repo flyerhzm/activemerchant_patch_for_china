5841a36ee1a991f441484e7e3d5aa7db03ad3338
Richard Huang <flyerhzm@gmail.com>
2013-03-17 15:45:02 +0800

    use openssl rather than ruby-hmac

--- lib/active_merchant/billing/integrations/yeepay/helper.rb
+++ lib/active_merchant/billing/integrations/yeepay/helper.rb
@@ -1,15 +1,15 @@
-require 'hmac-md5'
+require 'openssl'
 
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     module Integrations #:nodoc:
       module Yeepay
         class Helper < ActiveMerchant::Billing::Integrations::Helper
-          mapping :account, 'p1_MerId'       
+          mapping :account, 'p1_MerId'
           mapping :order, 'p2_Order'
           mapping :amount, 'p3_Amt'
           mapping :currency, 'p4_Cur'
-          
+
           mapping :description, 'p5_Pid'
           mapping :return_url, 'p8_Url'
           mapping :channel, 'pd_FrpId'
@@ -24,7 +24,7 @@ module ActiveMerchant #:nodoc:
           end
 
           def hmac_md5_hexdigest
-            HMAC::MD5.hexdigest(KEY, @fields.sort.collect{|s| s[1]}.join)
+            OpenSSL::HMAC.hexdigest("md5", KEY, @fields.sort.collect{|s| s[1]}.join)
           end
         end
       end
